d91557601f34035e352d38d77af58b85e8788e35
RelictRLCT <pigift@yandex.ru>
2024-09-24 13:21:38 +0300

    Новое имя

--- app/src/main/java/com/example/jokes_application/MainActivity.kt
+++ app/src/main/java/com/example/jokes_application/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Jokes_applicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Белошапка Евгений",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Привет, $name!",
         modifier = modifier
     )
 }
