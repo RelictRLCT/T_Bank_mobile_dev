046497528513c272ee5f721ce847859168347020
RELICT <pigift@yandex.ru>
2024-11-06 19:25:47 +0300

    Task 8

--- app/src/main/java/com/example/jokes_application/joke_detail_activity/JokeDetailActivity.kt
+++ app/src/main/java/com/example/jokes_application/joke_detail_activity/JokeDetailActivity.kt
@@ -1,52 +1,45 @@
-package com.example.jokes_application.joke_detail_activity
+package com.example.jokes_application.joke_detail
 
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
-import androidx.activity.ComponentActivity
+import androidx.fragment.app.Fragment
 import com.example.jokes_application.R
 
+class JokeDetailFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_joke_detail, container, false)
 
-class JokeDetailActivity : ComponentActivity() {
+        val category = arguments?.getString(EXTRA_CATEGORY)
+        val question = arguments?.getString(EXTRA_QUESTION)
+        val answer = arguments?.getString(EXTRA_ANSWER)
+
+        view.findViewById<TextView>(R.id.categoryDetailTextView).text = category
+        view.findViewById<TextView>(R.id.questionDetailTextView).text = question
+        view.findViewById<TextView>(R.id.answerDetailTextView).text = answer
+
+        return view
+    }
 
     companion object {
         private const val EXTRA_CATEGORY = "Category"
         private const val EXTRA_QUESTION = "Question"
         private const val EXTRA_ANSWER = "Answer"
 
-        fun createIntent(context: Context, category: String, question: String, answer: String): Intent {
-            return Intent(context, JokeDetailActivity::class.java).apply {
-                putExtra(EXTRA_CATEGORY, category)
-                putExtra(EXTRA_QUESTION, question)
-                putExtra(EXTRA_ANSWER, answer)
+        fun newInstance(category: String, question: String, answer: String): JokeDetailFragment {
+            val fragment = JokeDetailFragment()
+            fragment.arguments = Bundle().apply {
+                putString(EXTRA_CATEGORY, category)
+                putString(EXTRA_QUESTION, question)
+                putString(EXTRA_ANSWER, answer)
             }
+            return fragment
         }
-    }
-
-    private fun updateCategoryText(category: String?) {
-        findViewById<TextView>(R.id.categoryDetailTextView).text = category
-    }
-
-    private fun updateQuestionText(question: String?) {
-        findViewById<TextView>(R.id.questionDetailTextView).text = question
-    }
-
-    private fun updateAnswerText(answer: String?) {
-        findViewById<TextView>(R.id.answerDetailTextView).text = answer
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_joke_detail)
-
-        val category = intent.getStringExtra(EXTRA_CATEGORY)
-        val question = intent.getStringExtra(EXTRA_QUESTION)
-        val answer = intent.getStringExtra(EXTRA_ANSWER)
-
-        updateCategoryText(category)
-        updateQuestionText(question)
-        updateAnswerText(answer)
     }
 }
